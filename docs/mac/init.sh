550bccd89647712f4228450084a8f04564692cd3
Ilya Rusanen <ilya@rusanen.co.uk>
2019-01-02 02:41:48 +0300

    [feat] +docs

---
+++ docs/mac/init.sh
@@ -0,0 +1,15 @@
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew install python3 mas
+pip3 install ansible
+
+# ansible
+ansible-galaxy install geerlingguy.mas
+sudo mkdir /etc/ansible
+sudo curl https://init.f1nn.com/mac/ansible.hosts > /etc/ansible/hosts
+mkdir ~/tmp
+curl https://init.f1nn.com/mac/main.yml > ~/tmp/main.yml
+ansible-playbook ~/tmp/main.yml
+
--- init.sh
+++
@@ -1,7 +0,0 @@
-# install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install base apps
-brew install ansible
-
-# create config
