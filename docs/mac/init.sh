9ca0c641818295769fb270b16a1de7e1a04824bf
Ilya Rusanen <ilya@rusanen.co.uk>
2019-01-02 02:54:30 +0300

    [fix] inventory permissions

--- docs/mac/init.sh
+++ docs/mac/init.sh
@@ -9,7 +9,8 @@ pip3 install ansible
 # ansible
 ansible-galaxy install geerlingguy.mas
 sudo mkdir /etc/ansible
-sudo curl https://init51.f1nn.com/mac/ansible.hosts > /etc/ansible/hosts
+curl https://init51.f1nn.com/mac/ansible.hosts > ansible.hosts
+sudo mv ansible.hosts /etc/ansible/hosts
 mkdir ~/tmp
 curl https://init51.f1nn.com/mac/main.yml > ~/tmp/main.yml
 ansible-playbook ~/tmp/main.yml
